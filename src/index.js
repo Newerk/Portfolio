1815568b93f707839fd43f5800b7402ddf627c90
newerk <juniusowens@yahoo.com>
2023-08-03 12:51:13 -0400

    Fix issue of index.js Being Ran Twice

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import "./style.css"
 
-// let count = 0;
-// let test = document.body.querySelectorAll('.selection');
-// test.forEach(el => console.log(count++))
-
-// console.log(test);
+console.log('test')
