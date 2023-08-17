27ec83c720089717a8f7348ef51d61f31e8eb52a
newerk <juniusowens@yahoo.com>
2023-08-17 10:42:49 -0400

    Updates

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+import { checkScrollValue } from "./relocate-name"
+
+// const link = document.createElement('link');
+// link.rel = 'stylesheet';
+// link.href = '../style.css';
+// document.head.appendChild(link);
+
+checkScrollValue();
